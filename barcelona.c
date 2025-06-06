ba3ceb9c5f1afc80060570276798b878c54b0086
Jovan21122 <jovan.asdjanin@gmail.com>
2025-06-06 10:19:39 +0200

    Update barcelona.c

--- barcelona.c
+++ barcelona.c
@@ -1,12 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include "barcelona.h"
-
-
 
 void ucitajIgrace(Igrac igraci[], int* broj) {
     FILE* file = fopen("igraci.txt", "r");
@@ -28,9 +25,6 @@ void ucitajIgrace(Igrac igraci[], int* broj) {
             igrac.asistencije = 0;
             igraci[(*broj)++] = igrac;
         }
-        else {
-            printf("Preskacem neispravnu liniju: %s\n", linija);
-        }
     }
 
     fclose(file);
@@ -48,7 +42,6 @@ void ucitajStatistiku(Igrac igraci[], int broj) {
         int id, golovi, asistencije;
         char ime[50];
         if (sscanf(linija, " %d ; %49[^;] ; %d ; %d", &id, ime, &golovi, &asistencije) == 4) {
-
             for (int i = 0; i < broj; i++) {
                 if (igraci[i].id == id) {
                     igraci[i].golovi = golovi;
@@ -61,8 +54,6 @@ void ucitajStatistiku(Igrac igraci[], int broj) {
 
     fclose(file);
 }
-    
-
 
 void prikaziIgrace(Igrac igraci[], int broj) {
     printf("\n--- Igraci ---\n");
@@ -78,24 +69,17 @@ void prikaziStatistiku(Igrac igraci[], int broj) {
     }
 }
 
-
-int usporedbaGolova(const void* a, const void* b) {
-    return ((Igrac*)b)->golovi - ((Igrac*)a)->golovi;
-}
-void sortirajPoGolovima(Igrac igraci[], int broj) {
-    qsort(igraci, broj, sizeof(Igrac), usporedbaGolova);
-}
-
-int usporedbaAsistencija(const void* a, const void* b) {
-    return ((Igrac*)b)->asistencije - ((Igrac*)a)->asistencije;
-}
-void sortirajPoAsistencijama(Igrac igraci[], int broj) {
-    qsort(igraci, broj, sizeof(Igrac), usporedbaAsistencija);
-}
-
 void prikaziNajboljeStrijelce(Igrac igraci[], int broj) {
     printf("\n--- Najbolji strijelci ---\n");
-    sortirajPoGolovima(igraci, broj);
+    for (int i = 0; i < broj; i++) {
+        for (int j = i + 1; j < broj; j++) {
+            if (igraci[j].golovi > igraci[i].golovi) {
+                Igrac temp = igraci[i];
+                igraci[i] = igraci[j];
+                igraci[j] = temp;
+            }
+        }
+    }
     for (int i = 0; i < broj; i++) {
         printf("%s - %d golova\n", igraci[i].ime, igraci[i].golovi);
     }
@@ -103,7 +87,15 @@ void prikaziNajboljeStrijelce(Igrac igraci[], int broj) {
 
 void prikaziNajboljeAsistente(Igrac igraci[], int broj) {
     printf("\n--- Najbolji asistenti ---\n");
-    sortirajPoAsistencijama(igraci, broj);
+    for (int i = 0; i < broj; i++) {
+        for (int j = i + 1; j < broj; j++) {
+            if (igraci[j].asistencije > igraci[i].asistencije) {
+                Igrac temp = igraci[i];
+                igraci[i] = igraci[j];
+                igraci[j] = temp;
+            }
+        }
+    }
     for (int i = 0; i < broj; i++) {
         printf("%s - %d asistencija\n", igraci[i].ime, igraci[i].asistencije);
     }
@@ -125,38 +117,32 @@ void prikaziTrofeje() {
     fclose(file);
 }
 
+void ispisiIgracePoPoziciji(Igrac igraci[], int broj) {
+    const char* pozicije[] = { "Golman", "Branic", "Vezni", "Napadac" };
+    int n = sizeof(pozicije) / sizeof(pozicije[0]);
 
-int usporediImena(const void* a, const void* b) {
-    Igrac* igrac1 = (Igrac*)a;
-    Igrac* igrac2 = (Igrac*)b;
-    return _stricmp(igrac1->ime, igrac2->ime);
+    printf("\n--- Igraci po pozicijama ---\n");
+    for (int p = 0; p < n; p++) {
+        printf("\n%s:\n", pozicije[p]);
+        for (int i = 0; i < broj; i++) {
+            if (_stricmp(igraci[i].pozicija, pozicije[p]) == 0) {
+                printf("%s\n", igraci[i].ime);
+            }
+        }
+    }
 }
 
-
-Igrac* pretraziIgracaPoImenu(Igrac igraci[], int broj, const char* ime) {
-    sortirajPoGolovima(igraci, broj);
-    Igrac temp;
-    strncpy(temp.ime, ime, sizeof(temp.ime));
-    return (Igrac*)bsearch(&temp, igraci, broj, sizeof(Igrac), usporediImena);
-}
-
-
-void ispisiIgraceRekurzivno(Igrac igraci[], int indeks, int broj) {
-    if (indeks >= broj) return;
-    printf("%d. %s - %s\n", igraci[indeks].id, igraci[indeks].ime, igraci[indeks].pozicija);
-    ispisiIgraceRekurzivno(igraci, indeks + 1, broj);
-}
-
-void preimenujStatistiku() {
-    if (rename("statistika.txt", "stara_statistika.txt") == 0)
-        printf("Datoteka preimenovana.\n");
-    else
-        perror("Greska pri preimenovanju.");
-}
-
-void obrisiTrofeje() {
-    if (remove("trofeji.txt") == 0)
-        printf("Datoteka trofeji.txt je obrisana.\n");
-    else
-        perror("Greska pri brisanju.");
+void pretraziIgracaPoImenu(Igrac igraci[], int broj, const char* ime) {
+    int found = 0;
+    for (int i = 0; i < broj; i++) {
+        if (_stricmp(igraci[i].ime, ime) == 0) {
+            printf("Pronadjen: %s - %s, Golovi: %d, Asistencije: %d\n",
+                igraci[i].ime, igraci[i].pozicija, igraci[i].golovi, igraci[i].asistencije);
+            found = 1;
+            break;
+        }
+    }
+    if (!found) {
+        printf("Igrac nije pronadjen.\n");
+    }
 }
