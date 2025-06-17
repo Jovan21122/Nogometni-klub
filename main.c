d0dc36ffa8166a68f50ed0743417b07e2f4b8efb
Jovan21122 <jovan.asdjanin@gmail.com>
2025-06-17 02:43:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,34 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "barcelona.h"
 
-void menu() {
-    Igrac igraci[MAX_IGRACA];
-    int broj = 0;
+typedef enum {
+    IZLAZ,
+    PRIKAZ_IGRACA,
+    STATISTIKA,
+    STRIJELCI,
+    ASISTENTI,
+    TROFEJI,
+    PRETRAGA,
+    POZICIJE
+} Izbor;
 
-    ucitajIgrace(igraci, &broj);
-    ucitajStatistiku(igraci, broj);
+void menu() {
+    if (!ucitajIzBinarneDatoteke(igraci, &brojIgraca)) {
+        printf("Binarna datoteka nije pronadjena. Ucitavanje iz tekstualne...
+");
+        ucitajIzTekstualne();
+        spremiUBinarnu();
+    }
+    else {
+        printf("Podaci uspjesno ucitani iz binarne datoteke.
+");
+    }
 
     int izbor;
-    char imeZaPretragu[50];
+    char imeZaPretragu[30];
 
     do {
         printf("\n--- FC Barcelona Menu ---\n");
@@ -23,37 +41,42 @@ void menu() {
         printf("7. Ispis igraca po pozicijama\n");
         printf("0. Izlaz\n");
         printf("Izbor: ");
-        scanf("%d", &izbor);
+        if (scanf("%d", &izbor) != 1) break;
 
-        switch (izbor) {
-        case 1:
-            prikaziIgrace(igraci, broj);
-            break;
-        case 2:
-            prikaziStatistiku(igraci, broj);
-            break;
-        case 3:
-            prikaziNajboljeStrijelce(igraci, broj);
-            break;
-        case 4:
-            prikaziNajboljeAsistente(igraci, broj);
-            break;
-        case 5:
-            prikaziTrofeje();
-            break;
-        case 6:
-            printf("Unesi ime igraca: ");
-            scanf(" %[^\n]", imeZaPretragu);
-            pretraziIgracaPoImenu(igraci, broj, imeZaPretragu);
-            break;
-        case 7:
-            ispisiIgracePoPoziciji(igraci, broj);
-            break;
-        case 0:
-            printf("Izlaz iz programa.\n");
-            break;
-        default:
-            printf("Nepoznat izbor. Pokusaj ponovo.\n");
+        switch ((Izbor)izbor) {
+            case PRIKAZ_IGRACA:
+                ispisiRekurzivno(0);
+                break;
+            case STATISTIKA:
+                prikaziStatistiku(igraci, brojIgraca);
+                break;
+            case STRIJELCI:
+                sortirajPoGolovima();
+                ispisiRekurzivno(0);
+                break;
+            case ASISTENTI:
+                sortirajPoAsistencijama();
+                ispisiRekurzivno(0);
+                break;
+            case TROFEJI:
+                prikaziTrofeje();
+                break;
+            case PRETRAGA:
+                printf("Unesi ime igraca: ");
+                scanf(" %[^
+]", imeZaPretragu);
+                pretraziPoImenu(imeZaPretragu);
+                break;
+            case POZICIJE:
+                ispisiIgracePoPoziciji(igraci, brojIgraca);
+                break;
+            case IZLAZ:
+                spremiUBinarnu();
+                brisiMemoriju();
+                printf("Podaci spremljeni. Izlaz iz programa.\n");
+                break;
+            default:
+                printf("Nepoznat izbor. Pokusaj ponovo.\n");
         }
     } while (izbor != 0);
 }
