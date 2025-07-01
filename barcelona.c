baa2989c993c4a2530e74f61de90d1a0fb05bd1b
Jovan21122 <jovan.asdjanin@gmail.com>
2025-07-01 03:01:19 +0200

    Update barcelona.c

--- barcelona.c
+++ barcelona.c
@@ -21,11 +21,10 @@ int ucitajIzBinarneDatoteke(Igrac igraci_local[], int* broj) {
     fread(broj, sizeof(int), 1, file);
     igraci = malloc(*broj * sizeof(Igrac));
     if (!igraci) {
-        perror("Greska pri alokaciji memorije");
+        perror("Alokacija neuspješna");
         fclose(file);
         return 0;
     }
-
     fread(igraci, sizeof(Igrac), *broj, file);
     fclose(file);
     return 1;
@@ -33,49 +32,21 @@ int ucitajIzBinarneDatoteke(Igrac igraci_local[], int* broj) {
 
 void spremiUBinarnuDatoteku(Igrac igraci[], int broj) {
     FILE* file = fopen("igraci.dat", "wb");
-    if (!file) {
-        perror("Greska pri spremanju u binarnu datoteku");
-        return;
-    }
+    if (!file) return;
     fwrite(&broj, sizeof(int), 1, file);
     fwrite(igraci, sizeof(Igrac), broj, file);
     fclose(file);
 }
 
 void spremiUBinarnu() {
-    FILE* file = fopen("igraci.dat", "wb");
-    if (!file) {
-        perror("Greska pri spremanju u binarnu datoteku");
-        return;
-    }
-    fwrite(&brojIgraca, sizeof(int), 1, file);
-    fwrite(igraci, sizeof(Igrac), brojIgraca, file);
-    fclose(file);
-}
-
-void ucitajIzTekstualne() {
-    FILE* f = fopen("igraci.txt", "r");
-    if (!f) { perror("Greska otvaranja"); return; }
-
-    Igrac temp;
-    while (fscanf(f, "%d;%29[^;];%d;%d;%d;%d\n", &temp.id, temp.ime,
-        &temp.dob, (int*)&temp.pozicija, &temp.golovi, &temp.asistencije) == 6) {
-        void* novi = realloc(igraci, (brojIgraca + 1) * sizeof(Igrac));
-        if (!novi) {
-            perror("realloc failed");
-            exit(EXIT_FAILURE);
-        }
-        igraci = novi;
-        igraci[brojIgraca++] = temp;
-    }
-    fclose(f);
+    spremiUBinarnuDatoteku(igraci, brojIgraca);
 }
 
 void dodajIgraca() {
     void* novi = realloc(igraci, (brojIgraca + 1) * sizeof(Igrac));
     if (!novi) {
-        perror("Greska pri alokaciji memorije");
-        exit(EXIT_FAILURE);
+        perror("realloc failed");
+        return;
     }
     igraci = novi;
 
@@ -83,17 +54,17 @@ void dodajIgraca() {
     noviIgrac.id = brojIgraca + 1;
 
     printf("Unesi ime igraca: ");
-    if (scanf(" %29[^\n]", noviIgrac.ime) != 1) return;
+    scanf(" %29[^\n]", noviIgrac.ime);
     printf("Unesi dob: ");
-    if (scanf("%d", &noviIgrac.dob) != 1) return;
+    scanf("%d", &noviIgrac.dob);
     printf("Pozicija (1-Vratar, 2-Obrana, 3-Vezni, 4-Napad): ");
     int p;
-    if (scanf("%d", &p) != 1) return;
+    scanf("%d", &p);
     noviIgrac.pozicija = (Pozicija)p;
     printf("Golovi: ");
-    if (scanf("%d", &noviIgrac.golovi) != 1) return;
+    scanf("%d", &noviIgrac.golovi);
     printf("Asistencije: ");
-    if (scanf("%d", &noviIgrac.asistencije) != 1) return;
+    scanf("%d", &noviIgrac.asistencije);
 
     igraci[brojIgraca++] = noviIgrac;
 }
@@ -101,142 +72,40 @@ void dodajIgraca() {
 void azurirajIgraca() {
     int id;
     printf("Unesi ID igraca za azuriranje: ");
-    if (scanf("%d", &id) != 1) return;
+    scanf("%d", &id);
+
     for (int i = 0; i < brojIgraca; i++) {
         if (igraci[i].id == id) {
             printf("Novo ime: ");
-            if (scanf(" %29[^\n]", igraci[i].ime) != 1) return;
+            scanf(" %29[^\n]", igraci[i].ime);
             printf("Nova dob: ");
-            if (scanf("%d", &igraci[i].dob) != 1) return;
+            scanf("%d", &igraci[i].dob);
             printf("Novi broj golova: ");
-            if (scanf("%d", &igraci[i].golovi) != 1) return;
+            scanf("%d", &igraci[i].golovi);
             printf("Novi broj asistencija: ");
-            if (scanf("%d", &igraci[i].asistencije) != 1) return;
+            scanf("%d", &igraci[i].asistencije);
             return;
         }
     }
-    printf("Igrac s ID %d nije pronaden.\n", id);
+    printf("Igrac nije pronadjen.\n");
 }
 
 void obrisiIgraca() {
     int id;
     printf("Unesi ID igraca za brisanje: ");
-    if (scanf("%d", &id) != 1) return;
+    scanf("%d", &id);
+
     for (int i = 0; i < brojIgraca; i++) {
         if (igraci[i].id == id) {
-            for (int j = i; j < brojIgraca - 1; j++) {
+            for (int j = i; j < brojIgraca - 1; j++)
                 igraci[j] = igraci[j + 1];
-            }
             brojIgraca--;
-            void* novi = realloc(igraci, brojIgraca * sizeof(Igrac));
-            if (!novi && brojIgraca > 0) {
-                perror("realloc failed");
-                exit(EXIT_FAILURE);
-            }
-            igraci = novi;
+            igraci = realloc(igraci, brojIgraca * sizeof(Igrac));
             printf("Igrac obrisan.\n");
             return;
         }
     }
-    printf("Igrac nije pronaden.\n");
-}
-
-void sortirajPoGolovima() {
-    qsort(igraci, brojIgraca, sizeof(Igrac), usporedbaGolova);
-}
-
-void sortirajPoAsistencijama() {
-    qsort(igraci, brojIgraca, sizeof(Igrac), usporedbaAsistencija);
-}
-
-int usporedbaGolova(const void* a, const void* b) {
-    return ((Igrac*)b)->golovi - ((Igrac*)a)->golovi;
-}
-
-int usporedbaAsistencija(const void* a, const void* b) {
-    return ((Igrac*)b)->asistencije - ((Igrac*)a)->asistencije;
-}
-
-int usporedbaImena(const void* a, const void* b) {
-    return strcmp(((Igrac*)a)->ime, ((Igrac*)b)->ime);
-}
-
-void sortirajPoImenu() {
-    qsort(igraci, brojIgraca, sizeof(Igrac), usporedbaImena);
-}
-
-void pretraziPoImenu(const char* ime) {
-    for (int i = 0; i < brojIgraca; i++) {
-        if (strcmp(igraci[i].ime, ime) == 0) {
-            printf("Pronaden: %s, %d god, Pozicija: %s, Golovi: %d, Asistencije: %d\n",
-                igraci[i].ime, igraci[i].dob, nazivPozicije(igraci[i].pozicija), igraci[i].golovi, igraci[i].asistencije);
-            return;
-        }
-    }
-    printf("Nema igraca s tim imenom.\n");
-}
-
-void ispisiRekurzivno(int index) {
-    if (index >= brojIgraca) return;
-    printf("%d. %s, %d god, Pozicija: %s, Golovi: %d, Asistencije: %d\n",
-        igraci[index].id, igraci[index].ime, igraci[index].dob,
-        nazivPozicije(igraci[index].pozicija), igraci[index].golovi, igraci[index].asistencije);
-    ispisiRekurzivno(index + 1);
-}
-
-void preimenujDatoteku(const char* staro, const char* novo) {
-    if (rename(staro, novo) != 0) {
-        perror("Greska pri preimenovanju");
-    }
-    else {
-        printf("Datoteka preimenovana.\n");
-    }
-}
-
-void obrisiDatoteku(const char* naziv) {
-    if (remove(naziv) != 0) {
-        perror("Greska pri brisanju");
-    }
-    else {
-        printf("Datoteka obrisana.\n");
-    }
-}
-
-void ispisiVelicinuBinDatoteke() {
-    FILE* f = fopen("igraci.dat", "rb");
-    if (!f) { perror("Greska otvaranja"); return; }
-    fseek(f, 0, SEEK_END);
-    long velicina = ftell(f);
-    fclose(f);
-    printf("Velicina binarne datoteke: %ld bajtova\n", velicina);
-}
-
-void prikaziStatistiku(Igrac igraci[], int broj) {
-    printf("\nStatistika igraca (2024/25):\n");
-    for (int i = 0; i < broj; i++) {
-        printf("%s - Golovi: %d, Asistencije: %d\n", igraci[i].ime, igraci[i].golovi, igraci[i].asistencije);
-    }
-}
-
-void prikaziTrofeje() {
-    printf("\n--- Trofeji FC Barcelone ---\n");
-    printf("La Liga: 27\n");
-    printf("Copa del Rey: 31\n");
-    printf("Liga prvaka: 5\n");
-    printf("Superkup Europe: 5\n");
-    printf("Klupsko SP: 3\n");
-}
-
-void ispisiIgracePoPoziciji(Igrac igraci[], int broj) {
-    printf("\n--- Igraci po pozicijama ---\n");
-    for (int p = VRATAR; p <= NAPAD; p++) {
-        printf("\nPozicija: %s\n", nazivPozicije((Pozicija)p));
-        for (int i = 0; i < broj; i++) {
-            if (igraci[i].pozicija == p) {
-                printf("- %s\n", igraci[i].ime);
-            }
-        }
-    }
+    printf("Igrac nije pronadjen.\n");
 }
 
 void brisiMemoriju() {
@@ -245,3 +114,79 @@ void brisiMemoriju() {
     brojIgraca = 0;
 }
 
+void sortirajPoGolovima() {
+    qsort(igraci, brojIgraca, sizeof(Igrac), usporedbaGolova);
+}
+void sortirajPoAsistencijama() {
+    qsort(igraci, brojIgraca, sizeof(Igrac), usporedbaAsistencija);
+}
+void sortirajPoImenu() {
+    qsort(igraci, brojIgraca, sizeof(Igrac), usporedbaImena);
+}
+
+int usporedbaGolova(const void* a, const void* b) {
+    return ((Igrac*)b)->golovi - ((Igrac*)a)->golovi;
+}
+int usporedbaAsistencija(const void* a, const void* b) {
+    return ((Igrac*)b)->asistencije - ((Igrac*)a)->asistencije;
+}
+int usporedbaImena(const void* a, const void* b) {
+    return strcmp(((Igrac*)a)->ime, ((Igrac*)b)->ime);
+}
+
+void pretraziPoImenu(const char* ime) {
+    for (int i = 0; i < brojIgraca; i++) {
+        if (strcmp(igraci[i].ime, ime) == 0) {
+            printf("Pronaden: %s (%d god) | Pozicija: %s | Golovi: %d | Asistencije: %d\n",
+                igraci[i].ime, igraci[i].dob, nazivPozicije(igraci[i].pozicija),
+                igraci[i].golovi, igraci[i].asistencije);
+            return;
+        }
+    }
+    printf("Nema igraca s tim imenom.\n");
+}
+
+void ispisiRekurzivno(int index) {
+    if (index >= brojIgraca) return;
+    printf("%d. %s, %d god, %s, Golovi: %d, Asistencije: %d\n",
+        igraci[index].id, igraci[index].ime, igraci[index].dob,
+        nazivPozicije(igraci[index].pozicija),
+        igraci[index].golovi, igraci[index].asistencije);
+    ispisiRekurzivno(index + 1);
+}
+
+void ispisiIgracePoPoziciji(Igrac igraci[], int broj) {
+    for (int p = VRATAR; p <= NAPAD; p++) {
+        printf("\n--- %s ---\n", nazivPozicije((Pozicija)p));
+        for (int i = 0; i < broj; i++) {
+            if (igraci[i].pozicija == p)
+                printf("%s\n", igraci[i].ime);
+        }
+    }
+}
+
+void prikaziStatistiku(Igrac igraci[], int broj) {
+    printf("\n--- Statistika (2024/25) ---\n");
+    for (int i = 0; i < broj; i++) {
+        printf("%s - Golovi: %d, Asistencije: %d\n", igraci[i].ime, igraci[i].golovi, igraci[i].asistencije);
+    }
+}
+
+void prikaziTrofeje() {
+    printf("\n--- Trofeji FC Barcelone ---\n");
+    printf("La Liga: 27\nCopa del Rey: 31\nLiga prvaka: 5\nSuperkup Europe: 5\nKlupsko SP: 3\n");
+}
+
+void ispisiVelicinuBinDatoteke() {
+    FILE* file = fopen("igraci.dat", "rb");
+    if (!file) {
+        perror("Ne mogu otvoriti datoteku");
+        return;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long velicina = ftell(file);
+    fclose(file);
+
+    printf("Velicina binarne datoteke: %ld bajtova\n", velicina);
+}
